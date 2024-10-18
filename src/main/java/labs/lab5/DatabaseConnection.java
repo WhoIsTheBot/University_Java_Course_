406f55c95c9a56443c1c67a2f03262584bb2a4da
Vasyl <166917326+WhoIsTheBot@users.noreply.github.com>
2024-10-18 18:40:52 +0300

    Update DatabaseConnection.java

--- src/main/java/labs/lab5/DatabaseConnection.java
+++ src/main/java/labs/lab5/DatabaseConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/JavaCourse";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "Vasia#20J056";
+    private static final String URL = "****";
+    private static final String USER = "****";
+    private static final String PASSWORD = "****";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
